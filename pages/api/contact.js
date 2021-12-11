109bc3a048ecce3f92f761655deeccccac94b3bd
JoseIgnacioTapia <josignaciot@gmail.com>
2021-12-11 20:26:44 -0300

    Adding The Contact API Route

---
+++ pages/api/contact.js
@@ -0,0 +1,34 @@
+// /api/contact
+
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.include('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid Input' });
+      return;
+    }
+
+    // Store it in database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+
+    res
+      .status(201)
+      .json({ message: 'Successfully stored message', message: newMessage });
+  }
+}
+
+export default handler;
